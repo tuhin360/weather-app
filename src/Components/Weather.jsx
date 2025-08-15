3ef4f8943a8862cefa4992ec45b894ab4fed073f
tuhin <tuhin153695@gmail.com>
2025-08-15 19:28:32 +0600

    project starter initial

---
+++ src/Components/Weather.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Weather = () => {
+    return (
+        <div className='container'>
+           <div className="city">
+            <input type="text" placeholder='Enter City Name' />
+           </div>
+        </div>
+    );
+};
+
+export default Weather;
